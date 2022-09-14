f015f4dd82c099ee12edd012a748eafa03883004
airidasbudre <budreckis.airidas@gmail.com>
2022-09-14 18:52:23 +0300

    Update BankerRoulette.py

--- BankerRoulette.py
+++ BankerRoulette.py
@@ -1,13 +1,14 @@
 import random
 
-list1 = []
-
 
 people = input("Write all names: ")
 group_of_people = people.split(",")
 
-list2 = list1.extend(group_of_people)
+number = len(group_of_people)
 
-a = str(random.random(group_of_people))
+a = random.randint(0, number)
 
-print(f"For food should pay {a}")
+c = group_of_people[a]
+
+
+print(f"For food should pay {c}")
