2ebe9b631fc68d5a1f8bf0f708b16fca41c007cc
airidasbudre <107766475+airidasbudre@users.noreply.github.com>
2022-11-21 20:32:22 +0200

    Add files via upload

--- FizzBuzz.game.py
+++ FizzBuzz.game.py
@@ -1,11 +1,8 @@
 for number in range(1, 101):
     if number % 3 == 0 and number % 5 == 0:
         number = 'FizzBuzz'
-        #print("Fizz")
     elif number % 5 == 0:
         number = 'Buzz'
-        #print("Buzz")
     elif number % 3 == 0:
         number = 'Fizz'
-        #print("FizzBuzz")
     print(number)
